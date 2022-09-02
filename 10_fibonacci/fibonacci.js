b8cb9797d94d91962e1f63c003e55517015e8018
Andres Soria <andres.zov@gmail.com>
2022-09-02 17:17:52 -0500

    Add solution to exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
+  let upTo = parseInt(num);
+  let n1 = 0,
+    n2 = 1,
+    nextTerm;
+  nextTerm = n1 + n2;
+  for (let i = 0; i < upTo; i++) {
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+  }
+  return n1;
 };
 
 // Do not edit below this line
