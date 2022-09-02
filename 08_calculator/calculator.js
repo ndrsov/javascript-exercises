4801dc919a718aab79494f138a8293a7302bc697
Andres Soria <andres.zov@gmail.com>
2022-09-02 15:05:24 -0500

    Add solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  const initialValue = 0;
+  return arr.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((prev, curr) => prev * curr);
 };
 
-const power = function() {
-	
+const power = function (num, to) {
+  let result = num;
+  for (let i = 1; i < to; i++) {
+    result *= num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
